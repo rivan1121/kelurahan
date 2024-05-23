7736db080b7d66fea8570500db9514e58648ee4f
MOHAMAD RIVAN AGUSTIAN <127384859+rivan1121@users.noreply.github.com>
2024-05-23 15:44:00 +0700

    script.js

--- script.js
+++ script.js
@@ -14,3 +14,11 @@ function initMap() {
         title: 'Kelurahan Tanjung, Tasikmalaya'
     });
 }
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('.nav-menu');
+
+    hamburger.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+    });
+});
